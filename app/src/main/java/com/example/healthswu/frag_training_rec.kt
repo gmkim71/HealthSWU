1594b827f45c8f69cb51945a8cdb37b4f0aaffaa
ddb <81467937+Kim-ge@users.noreply.github.com>
2023-07-27 22:31:28 +0900

    Add files via upload

---
+++ app/src/main/java/com/example/healthswu/frag_training_rec.kt
@@ -0,0 +1,51 @@
+package com.example.guru
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class frag_training_rec : Fragment() {
+
+    companion object {
+        fun newInstance() = frag_training_rec()
+    }
+
+    private lateinit var viewModel: FragTrainingRecViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_frag_training_rec, container, false)
+        setHasOptionsMenu(true)
+        return view
+    }
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(FragTrainingRecViewModel::class.java)
+        // TODO: Use the ViewModel
+
+    }
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
